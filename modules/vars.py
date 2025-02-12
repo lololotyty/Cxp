47479b8b9ffac95bf323b274827ca14b1f191d61
Aalu-ka-Chalu <sainisainiriya002@gmail.com>
2025-02-12 13:27:15 +0530

    vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,6 +3,5 @@ import os
 api_id    = int(os.environ.get("api_id", "25579552"))
 api_hash  = os.environ.get("api_hash", "ac24e438ff9a0f600cf3283e6d60b1aa")
 bot_token = os.environ.get("bot_token", "7430383550:AAHevTpQweWPhesbRKFUbaV4Syha1T5vu-A")
-x_access_token = os.getenv("x_access_token", "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA1MzQ2MDg2LCJvcmdJZCI6MzU4OTY5LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTYyODkzMTI2NjIiLCJuYW1lIjoiQW5hbnQgSmhhIiwiZW1haWwiOm51bGwsImlzSW50ZXJuYXRpb25hbCI6MCwiZGVmYXVsdExhbmd1YWdlIjoiRU4iLCJjb3VudHJ5Q29kZSI6IklOIiwiY291bnRyeUlTTyI6IjkxIiwidGltZXpvbmUiOiJHTVQrNTozMCIsImlzRGl5Ijp0cnVlLCJvcmdDb2RlIjoicXFhamsiLCJpc0RpeVN1YmFkbWluIjowLCJmaW5nZXJwcmludElkIjoiYzgyMTI2NGRlYmxrOGZiM2UyYTYwYmRiZDRlZjcyYTNiOSIsImlhdCI6MTczOTM0NDY0MCwiZXhwIjoxNzM5OTQ5NDQwfQ.sjQBjCcQSLZzKDDZujrfcwYjGzyPzcC_q1QBdMgJaMAbqK1zcKfOILbugozOI_ZX")
 AUTH = "7548265642"
 #Bot Username = "MARCO_015"
