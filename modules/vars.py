0d1d0bc60c3e5e206baacf59ff4d665dd2176c6d
Aalu-ka-Chalu <sainisainiriya002@gmail.com>
2025-02-12 12:53:24 +0530

    Add files via upload

---
+++ modules/vars.py
@@ -0,0 +1,8 @@
+import os
+
+api_id    = int(os.environ.get("api_id", "25579552"))
+api_hash  = os.environ.get("api_hash", "ac24e438ff9a0f600cf3283e6d60b1aa")
+bot_token = os.environ.get("bot_token", "7618636339:AAFmRavUpPQ7LuV4mPsDzpT1qhu-1NUMvyU")
+pwtoken = os.getenv("pwtoken", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mzk2MDAyNDAuODIxLCJkYXRhIjp7Il9pZCI6IjY0YTEwMTJlMWNmOWI0MDAxOGI1MTQxZCIsInVzZXJuYW1lIjoiNjIwNzM4NDEwNyIsImZpcnN0TmFtZSI6IkFqIFB5dGhvbiIsImxhc3ROYW1lIjoiSkhBIiwib3JnYW5pemF0aW9uIjp7Il9pZCI6IjVlYjM5M2VlOTVmYWI3NDY4YTc5ZDE4OSIsIndlYnNpdGUiOiJwaHlzaWNzd2FsbGFoLmNvbSIsIm5hbWUiOiJQaHlzaWNzd2FsbGFoIn0sImVtYWlsIjoiYWpweXRob24yMjJAZ21haWwuY29tIiwicm9sZXMiOlsiNWIyN2JkOTY1ODQyZjk1MGE3NzhjNmVmIl0sImNvdW50cnlHcm91cCI6IklOIiwidHlwZSI6IlVTRVIifSwiaWF0IjoxNzM4OTk1NDQwfQ.EokNiH4aiaGrZgJVBnyGU0R94BgT-_tfg4ZqPwx1a04")
+AUTH = "7548265642"
+#Bot Username = "MARCO_015"
