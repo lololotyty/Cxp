41438f5e247f46552ccc1fa3f9b2528a0b101cc4
wddwwdw <hangerskype@gmail.com>
2025-02-15 15:15:55 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,6 +2,6 @@ import os
 
 api_id    = int(os.environ.get("api_id", "25579552"))
 api_hash  = os.environ.get("api_hash", "ac24e438ff9a0f600cf3283e6d60b1aa")
-bot_token = os.environ.get("bot_token", "7430383550:AAHevTpQweWPhesbRKFUbaV4Syha1T5vu-A")
-AUTH = "7548265642"
-#Bot Username = "MARCO_015"
+bot_token = os.environ.get("bot_token", "")
+AUTH = "6820248376"
+#Bot Username = "Captionchanger_bot"
